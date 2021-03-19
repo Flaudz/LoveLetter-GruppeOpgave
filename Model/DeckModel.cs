37cabbc7bb5c3a69c7adb7b7cf5365e2d1f05a7c
Nicolaj Thygesen <nico936d@edu.campusvejle.dk>
2021-03-19 10:39:19 +0100

    Made all models and some viewmodels

--- Model/DeckModel.cs
+++ Model/DeckModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace LoveLetter_GruppeOpgave.Model
 {
-    class DeckModel
+    public class DeckModel
     {
+        private ObservableCollection<Card> deck;
+
+        public ObservableCollection<Card> Deck { get => deck; set => deck = value; }
     }
 }
--- Model/LobbyModel.cs
+++ Model/LobbyModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace LoveLetter_GruppeOpgave.Model
 {
-    class LobbyModel
+    public class Lobby
     {
+        private int id;
+        private Player lobbyLeader;
+        private int numberOfPlayers;
+        private ObservableCollection<Player> players;
+
+        public int Id { get => id; set => id = value; }
+        public Player LobbyLeader { get => lobbyLeader; set => lobbyLeader = value; }
+        public int NumberOfPlayers { get => numberOfPlayers; set => numberOfPlayers = value; }
+        public ObservableCollection<Player> Players { get => players; set => players = value; }
     }
 }
