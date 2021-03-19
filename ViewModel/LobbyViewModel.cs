48523bea77d0c2560d2ac6e26caaca7c1dbc7ad3
Nicolaj Thygesen <nico936d@edu.campusvejle.dk>
2021-03-19 10:50:05 +0100

    Made something in viewmodels (Forgot to change branch)

--- ViewModel/LobbyViewModel.cs
+++ ViewModel/LobbyViewModel.cs
@@ -8,6 +8,7 @@ namespace LoveLetter_GruppeOpgave.ViewModel
 {
     public class LobbyViewModel
     {
+        Player MyPlayerObject = new Player(4, "Nicolai");
         public ObservableCollection<Lobby> Lobbies
         {
             get;
@@ -19,6 +20,18 @@ namespace LoveLetter_GruppeOpgave.ViewModel
             ObservableCollection<Lobby> lobbies = new ObservableCollection<Lobby>();
 
             lobbies.Add(new Lobby { Id = 1, LobbyLeader = new Player(1, "Nicolaj"), Players = new ObservableCollection<Player>{ new Player(1, "Nicolaj"), new Player(2, "Jimmy"), new Player(3, "Christian") }, NumberOfPlayers = 3 });
+        }
+
+        public void JoinLobby(Lobby lobby)
+        {
+            lobby.NumberOfPlayers++;
+            lobby.Players.Add(MyPlayerObject);
+        }
+
+        public void LeaveLobby(Lobby lobby)
+        {
+            lobby.NumberOfPlayers--;
+            lobby.Players.Remove(MyPlayerObject);
         }
 
     }
