95bbe573a6928e9f5c5de8f5bc2c3e9beb2be9fc
Nicolaj Thygesen <nico936d@edu.campusvejle.dk>
2021-03-25 11:51:09 +0100

    Made it work i think xD I wanna go home

--- Model/LobbyModel.cs
+++ Model/LobbyModel.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 
 namespace LoveLetter_GruppeOpgave.Model
 {
-    public class Lobby
+    public class Lobby : INotifyPropertyChanged
     {
+        // Fields
         private int id;
         private Player lobbyLeader;
         private int numberOfPlayers;
         private ObservableCollection<Player> players;
 
-        public int Id { get => id; set => id = value; }
-        public Player LobbyLeader { get => lobbyLeader; set => lobbyLeader = value; }
-        public int NumberOfPlayers { get => numberOfPlayers; set => numberOfPlayers = value; }
-        public ObservableCollection<Player> Players { get => players; set => players = value; }
-
+        // Constructor
         public Lobby(int id, Player lobbyLeader, int numberOfPlayers, ObservableCollection<Player> players)
         {
             this.id = id;
@@ -24,5 +22,61 @@ namespace LoveLetter_GruppeOpgave.Model
             this.numberOfPlayers = numberOfPlayers;
             this.players = players;
         }
+
+        // I dont Know
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id == value) return;
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public Player LobbyLeader
+        {
+            get { return lobbyLeader; }
+            set
+            {
+                if (lobbyLeader == value) return;
+                lobbyLeader = value;
+                OnPropertyChanged("LobbyLeader");
+            }
+        }
+        public int NumberOfPlayers
+        {
+            get { return numberOfPlayers; }
+            set
+            {
+                if (numberOfPlayers == value) return;
+                numberOfPlayers = value;
+                OnPropertyChanged("NumberOfPlayers");
+            }
+        }
+        public ObservableCollection<Player> Players
+        {
+            get { return players; }
+            set
+            {
+                if (players == value) return;
+                players = value;
+                OnPropertyChanged("Players");
+            }
+        }
+
+
+        // This is to update the gui when something has changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
